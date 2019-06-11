5c2e696d96ed42bfe431a7ebec9d8c81e3c5747f
Andrew McOlash <amcolash@gmail.com>
2019-06-10 17:29:11 -0700

    Add clock to bottom

--- bluetooth.js
+++ bluetooth.js
@@ -30,6 +30,18 @@ window.onload = () => {
             scope: window.location.pathname.replace('/index.html', '/')
         });
     }
+
+    setIntervalImmediately(() => {
+        // Got code from https://stackoverflow.com/questions/8888491/
+        let date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+        time.innerHTML = hours + ':' + minutes + ' ' + ampm;
+    }, 5000);
 }
 
 function setIntervalImmediately(callback, time) {
